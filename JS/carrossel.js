427ab4721a23ab6de63c438dcab0753d7d4a5abf
geovannadarosa <geovannadarosava@gmail.com>
2026-04-22 17:46:51 -0400

    atualização
    adicionando o js com carrossel infinito e nao infinito

---
+++ JS/carrossel.js
@@ -0,0 +1,15 @@
+let index = 0;
+const total =3;
+
+function update(){
+    const carrossel_principal = document.getElementById("carrossel-principal");
+    carrossel_principal.style.transform = `tranlateX(-${index * 100}%)` 
+}
+function next(){
+    index = (index + 1) % total;
+    update();
+}
+function prev(){
+    index = (index - 1) % total;
+    update();
+}
