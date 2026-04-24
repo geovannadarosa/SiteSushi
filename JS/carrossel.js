49b71892b83d836570b682d5609217787933258d
geovannadarosa <geovannadarosava@gmail.com>
2026-04-24 14:23:17 -0400

    terminando carrossel

--- JS/carrossel.js
+++ JS/carrossel.js
@@ -3,13 +3,13 @@ const total =3;
 
 function update(){
     const carrossel_principal = document.getElementById("carrossel-principal");
-    carrossel_principal.style.transform = `tranlateX(-${index * 100}%)` 
+    carrossel_principal.style.transform = `translateX(-${index * 100}%)`; 
 }
 function next(){
     index = (index + 1) % total;
     update();
 }
 function prev(){
-    index = (index - 1) % total;
+    index = (index - 1 + total) % total;
     update();
 }
